977846e8a36c1eaebd26570be2739ba5d9bba618
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2024-03-25 13:07:50 +0000

    workflows: drop permissive builds
    delete useless workflows

--- check-kernelsu-update.sh
+++ check-kernelsu-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config --local user.name "github-actions[bot]"
+#git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+#git config --local user.name "github-actions[bot]"
 git submodule update --init
 pushd KernelSU
 LOCAL_LATEST=$(git describe --tags --abbrev=0)
